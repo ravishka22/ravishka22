cdd16df0b06f6d5ab87dc47c42ea6102904a94c8
Ravishka Bhashitha <67957355+ravishka22@users.noreply.github.com>
2026-07-21 10:06:51 +0530

    Update today.py

--- today.py
+++ today.py
@@ -447,7 +447,7 @@ if __name__ == '__main__':
     user_data, user_time = perf_counter(user_getter, USER_NAME)
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
-    age_data, age_time = perf_counter(daily_readme, datetime.datetime(YYYY, M, D))  # TODO: replace YYYY, M, D with your birth year, month, day
+    age_data, age_time = perf_counter(daily_readme, datetime.datetime(2003, 2, 22))  # TODO: replace YYYY, M, D with your birth year, month, day
     formatter('age calculation', age_time)
     total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
